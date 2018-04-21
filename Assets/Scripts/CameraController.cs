07eef6913d5f9f92f4c1b4e28cad727eb9b3a7d0
Martin Zackrisson <martin.zackrisson@gmail.com>
2018-04-21 18:56:48 +0200

    Camera size by running speed

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    AnimationCurve speedToViewSize;
+
+    [SerializeField]
+    Pacer pacer;
+
+    [SerializeField, Range(0, 1)]
+    float attack;
+
+    Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        //Debug.Log(pacer.Speed);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, speedToViewSize.Evaluate(pacer.Speed), attack * Time.deltaTime);
+    }
+}

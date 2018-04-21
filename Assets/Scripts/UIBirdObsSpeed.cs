60775bb6add3f49319a5c1575d04f8277ffc064d
Martin Zackrisson <martin.zackrisson@gmail.com>
2018-04-21 18:38:15 +0200

    More UI

---
+++ Assets/Scripts/UIBirdObsSpeed.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBirdObsSpeed : MonoBehaviour {
+
+    [SerializeField] Text spedometer;
+    [SerializeField] Start start;
+    [SerializeField] Goal goal;
+
+    BirdWatching[] birds;
+
+    int observations;
+
+    private void OnEnable()
+    {
+        if (birds == null) birds = GameObject.FindObjectsOfType<BirdWatching>();
+        for (int i = 0; i < birds.Length; i++)
+        {
+            birds[i].OnBirdWatched += HandleObservation;
+        }
+        start.OnRunStart += HandleRunEvent;
+        goal.OnRunGoal += HandleRunEvent;
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < birds.Length; i++)
+        {
+            birds[i].OnBirdWatched -= HandleObservation;
+        }
+        start.OnRunStart -= HandleRunEvent;
+        goal.OnRunGoal -= HandleRunEvent;
+    }
+
+    private void HandleObservation(string species)
+    {
+        observations += 1;
+        if (startTime != 0) spedometer.text = GetFormattedSpeed(time);
+    }
+
+    float startTime;
+    float endTime;
+
+    void HandleRunEvent(RunPhase phase, float time)
+    {
+        if (phase == RunPhase.Start)
+        {
+            startTime = time;
+        }
+        else if (phase == RunPhase.Goal)
+        {
+            endTime = time;
+        }
+    }
+
+    void Update()
+    {
+        if (startTime == 0)
+        {
+            return;
+        }
+        spedometer.text = GetFormattedSpeed(time);
+    }
+
+
+    string GetFormattedSpeed(float time)
+    {
+        return (observations * 60 / time ).ToString("00.0");
+    }
+
+    float time
+    {
+        get
+        {
+            if (startTime == 0)
+            {
+                return 0;
+            }
+            else if (endTime == 0)
+            {
+                return Time.time - startTime;
+            }
+            else
+            {
+                return endTime - startTime;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/UIRunProgress.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIRunProgress : MonoBehaviour {
+
+    [SerializeField]
+    float totalDistance = 22.3f;
+
+    [SerializeField]
+    RunController rc;
+
+    [SerializeField]
+    RectTransform runner;
+
+    private void OnEnable()
+    {
+        rc.OnGPS += HandleGPS;
+    }
+
+    private void OnDisable()
+    {
+        rc.OnGPS -= HandleGPS;
+    }
+
+    bool hasStarted = false;
+    float distance = 0;
+    Vector2 lastPos;
+    float minAnchorY;
+    float maxAnchorY;
+
+    void Start()
+    {
+        minAnchorY = runner.anchorMin.y;
+        maxAnchorY = runner.anchorMax.y;
+    }
+
+    private void HandleGPS(Vector2 pos)
+    {
+        if (hasStarted)
+        {
+            distance += Vector2.Distance(lastPos, pos);
+            lastPos = pos;
+            float progress = Mathf.Clamp01(distance / totalDistance);
+            runner.anchorMax = new Vector2(progress, maxAnchorY);
+            runner.anchorMin = new Vector2(progress, minAnchorY);
+        }
+        else
+        {
+            lastPos = pos;
+            hasStarted = true;
+        }
+
+    }
+}

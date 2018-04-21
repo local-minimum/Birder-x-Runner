62b48fe009e8eae228cae66eec2e13420c06be42
Martin Zackrisson <martin.zackrisson@gmail.com>
2018-04-21 15:38:09 +0200

    Bird watching 101

---
+++ Assets/Scripts/BirdWatching.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BirdWatching : MonoBehaviour {
+
+    [SerializeField]
+    Image obsAlertImage;
+
+    [SerializeField]
+    Image obsProgressImage;
+
+    [SerializeField, Range(1, 2f)]
+    float hoverSpeedup = 1.25f;
+
+    [SerializeField, Range(0, 10)]
+    float obsDuration = 3f;
+
+    float obsTimeSoFar = 0f;
+
+    bool obsStarted = false;
+    bool hovered = false;
+
+    private void OnMouseEnter()
+    {
+        hovered = true;
+    }
+
+    private void OnMouseExit()
+    {
+        hovered = false;
+    }
+
+    private void Update()
+    {
+        if (obsStarted)
+        {
+            obsTimeSoFar += (hovered ? hoverSpeedup : 1f) * Time.deltaTime;
+            obsProgressImage.fillAmount = Mathf.Clamp01(obsTimeSoFar / obsDuration);
+            obsAlertImage.enabled = false;
+        } else if (hovered && Input.GetMouseButtonDown(0))
+        {
+            obsStarted = true;
+            obsAlertImage.enabled = true;
+        } else
+        {
+            obsAlertImage.enabled = hovered;
+        }
+    }
+}

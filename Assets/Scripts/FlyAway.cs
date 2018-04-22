af356e03701582547f466af8fd19366b5287462b
Martin Zackrisson <martin.zackrisson@gmail.com>
2018-04-22 15:32:30 +0200

    Fly away bird

---
+++ Assets/Scripts/FlyAway.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyAway : MonoBehaviour {
+
+    [SerializeField] GameObject bird;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        if (!escaping && collision.tag == "Player")
+        {
+            StartCoroutine(Escape());
+        }
+    }
+    [SerializeField] float flightTime;
+    [SerializeField] AnimationCurve elevation;
+    [SerializeField] AnimationCurve horizontal;
+
+    bool escaping = false;
+    IEnumerator<WaitForSeconds> Escape()
+    {
+        escaping = true;
+        float start = Time.time;
+        float duration = 0;
+        Vector3 refPos = bird.transform.localPosition;
+        while (duration < flightTime)
+        {
+            duration = Time.time - start;
+            bird.transform.localPosition = new Vector3(
+                refPos.x + horizontal.Evaluate(duration),
+                refPos.y + elevation.Evaluate(duration),
+                refPos.z
+            );
+            yield return new WaitForSeconds(0.02f);
+        }
+        bird.SetActive(false);
+    }
+}

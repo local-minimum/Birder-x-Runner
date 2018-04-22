818325a20dc45987d048c3c6d3797dbc4103d943
Martin Zackrisson <martin.zackrisson@gmail.com>
2018-04-22 08:49:52 +0200

    camera zoom fixed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,16 +13,28 @@ public class CameraController : MonoBehaviour {
     [SerializeField, Range(0, 1)]
     float attack;
 
+    [SerializeField]
+    AnimationCurve xOffset;
+
     Camera cam;
+
+    [SerializeField] AnimationCurve yOffset;
+    float zOffset;
 
     private void Start()
     {
         cam = GetComponent<Camera>();
+        zOffset = transform.localPosition.z;
     }
 
     private void Update()
     {
         //Debug.Log(pacer.Speed);
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, speedToViewSize.Evaluate(pacer.Speed), attack * Time.deltaTime);
+        Vector3 localPos = transform.localPosition;
+        localPos.x = Mathf.Lerp(localPos.x, xOffset.Evaluate(cam.orthographicSize), attack);
+        localPos.y = Mathf.Lerp(localPos.y, yOffset.Evaluate(cam.orthographicSize), attack);
+        localPos.z = zOffset;
+        transform.localPosition = localPos;
     }
 }

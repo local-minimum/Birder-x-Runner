7399388b5befa80a6af4a03eb6b16b43ec78fb21
Martin Zackrisson <martin.zackrisson@gmail.com>
2018-04-22 17:08:06 +0200

    Some scenes

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void LoadStory()
+    {
+        SceneManager.LoadScene("Scenes/story");
+    }
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene("Scenes/tutorial");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Scenes/credits");
+    }
+
+    public void LoadHighscore()
+    {
+        SceneManager.LoadScene("Scenes/highscore");
+    }
+
+    public void Load1k()
+    {
+        SceneManager.LoadScene("Scenes/1k");
+    }
+}
---
+++ Assets/Scripts/Story.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Story : MonoBehaviour {
+
+    [SerializeField]
+    Text txt;
+
+    [SerializeField]
+    string birdingStory = "";
+
+    [SerializeField]
+    string runningStory = "";
+
+    int step = 0;
+
+    private void Start()
+    {
+        txt.text = birdingStory;
+    }
+
+    void Update () {
+        if (Input.anyKeyDown)
+        {
+            if (step == 0)
+            {
+                GetComponent<Animator>().SetTrigger("Swap");
+                step += 1;
+                ShowRunningStory();
+            }
+            else
+            {
+                SceneManager.LoadScene("Scenes/menu");
+            }
+        }
+	}
+
+    public void ShowRunningStory()
+    {
+        txt.text = runningStory;
+    }
+}
